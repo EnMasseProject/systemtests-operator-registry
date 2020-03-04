274310b28801728eb8ff029f46cdddaf1b4dc2db
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-04 12:25:56 +0100

    Do registry replacement of manifest images

--- copy_manifests.sh
+++ copy_manifests.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
 id=$(docker create $1)
+REGISTRY=${2}
 docker cp $id:/manifests $PWD
+if [ "${REGISTRY}" != "" ]; then
+    find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7/amq-online-,${REGISTRY}/rh-osbs/amq7-amq-online-,g" -i
+    find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7-tech-preview/amq-online-,${REGISTRY}/rh-osbs/amq7-tech-preview-amq-online-,g" -i
+fi
 docker rm -v $id
