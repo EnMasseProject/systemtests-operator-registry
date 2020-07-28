495e63c27d4aeb09aa1bd4ed5dbdc52da7406261
Ondrej Babec <ond.babec@gmail.com>
2020-07-28 10:22:42 +0200

    Update copy manifests

--- copy_manifests.sh
+++ copy_manifests.sh
@@ -2,11 +2,13 @@
 id=$(docker create $1 /bin/bash)
 REGISTRY=${2}
 docker cp $id:/manifests $PWD
-if [[ "${REGISTRY}" != "" ]] && [[ "${REGISTRY}" != "image-registry.openshift-image-registry.svc:5000" ]]; then
-    find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7/amq-online-,${REGISTRY}/rh-osbs/amq7-amq-online-,g" -i
-    find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7-tech-preview/amq-online-,${REGISTRY}/rh-osbs/amq7-tech-preview-amq-online-,g" -i
-elif [[ "${REGISTRY}" == "image-registry.openshift-image-registry.svc:5000" ]]; then
-    find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7/amq-online-,${REGISTRY}/amq-online-images/amq-online-,g" -i
-    find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7-tech-preview/amq-online-,${REGISTRY}/amq-online-images/amq-online-,g" -i
+if [[ "${REGISTRY}" != "registry.redhat.io" ]]; then
+  if [[ "${REGISTRY}" != "" ]] && [[ "${REGISTRY}" != "image-registry.openshift-image-registry.svc:5000" ]]; then
+      find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7/amq-online-,${REGISTRY}/rh-osbs/amq7-amq-online-,g" -i
+      find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7-tech-preview/amq-online-,${REGISTRY}/rh-osbs/amq7-tech-preview-amq-online-,g" -i
+  elif [[ "${REGISTRY}" == "image-registry.openshift-image-registry.svc:5000" ]]; then
+      find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7/amq-online-,${REGISTRY}/amq-online-images/amq-online-,g" -i
+      find manifests -name "*.yaml" | xargs sed -e "s,registry.redhat.io/amq7-tech-preview/amq-online-,${REGISTRY}/amq-online-images/amq-online-,g" -i
+  fi
 fi
 docker rm -v $id
